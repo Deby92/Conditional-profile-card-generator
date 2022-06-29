8b0d70dce1a05ad86ed47855e005d270817c1548
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-06-29 15:17:55 +0000

    miercoles

--- src/js/app.js
+++ src/js/app.js
@@ -29,11 +29,13 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let name = "Lucy";
+  if (variables.name !== null) name = variables.name;
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${name} Boilett</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
